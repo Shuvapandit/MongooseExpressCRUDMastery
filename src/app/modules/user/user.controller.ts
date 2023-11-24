40f3328a91f9424459b50b2fd4c34dd73f5af46e
shuvapnadit <chakrabortyshuva216@gmail.com>
2023-11-24 23:31:44 +0600

    zod validation added

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from "express";
 import { userServices } from "./user.service";
+import userValidationSchema from "./user.validation";
+
 const createUser = async (req: Request, res: Response) => {
     try {
-
         const { user: userData } = req.body
-        const result = await userServices.createUserintoDB(userData)
-        res.status(200).json({
+        //schema validation using zod
+        const zodParsedData = userValidationSchema.parse(userData);
+        const result = await userServices.createUserintoDB(zodParsedData)
+        res.status(500).json({
             status: 'success',
             message: "User created successfully!",
             data: result,
---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,30 @@
+import { z } from 'zod';
+const addressValidationSchema = z.object({
+    street: z.string(),
+    city: z.string(),
+    country: z.string(),
+});
+
+const ordersValidationSchema = z.object({
+    productName: z.string(),
+    price: z.number(),
+    quantity: z.number(),
+});
+
+const usernameValidationSchema = z.object({
+    firstName: z.string().min(1).max(10),
+    lastName: z.string().min(1).max(10),
+});
+
+const userValidationSchema = z.object({
+    userId: z.number(),
+    password: z.string().min(1).max(20),
+    username: usernameValidationSchema,
+    age: z.number(),
+    email: z.string(),
+    isActive: z.enum(['active', 'blocked']).default('active'),
+    hobbies: z.enum(['Fishing', 'playing', 'Travelling']),
+    address: addressValidationSchema,
+    orders: ordersValidationSchema,
+});
+export default userValidationSchema;
